b6c0baae9b202f6141641f3481a67ce3c39eed55
Seenyo <naruseabc@gmailcom>
2024-04-11 16:10:19 +0900

    updated the histgram calculation

--- cpu_generate_particles2d_st.py
+++ cpu_generate_particles2d_st.py
@@ -9,13 +9,14 @@ import numpy as np
 import taichi as ti
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 from stqdm import stqdm
 from vispy import scene
 from vispy.io import write_png
 
 
-def toroidal_distance_cpu(length, p1, p2):
+def toroidal_distance(length, p1, p2):
     dx = abs(p2[0] - p1[0])
     dy = abs(p2[1] - p1[1])
 
@@ -45,6 +46,7 @@ class MetropolisHastings:
     @ti.kernel
     def initialize_particles(self):
 
+        # Old version of initializing particles
         # for particle_idx in range(self.num_of_particles):
         #     self.init_particles[particle_idx] = ti.Vector([ti.random(dtype=ti.f32), ti.random(dtype=ti.f32)])
 
@@ -80,7 +82,6 @@ class MetropolisHastings:
             denominator = (self.a ** 2 + 1) ** 3
             c_ab_val = -1 * numerator / denominator
 
-
             kappa2_37_sum = 0.0
 
             for k in range(self.num_of_particles):
@@ -92,64 +93,81 @@ class MetropolisHastings:
 
             acceptance_ratio = 1 / (1 ** self.num_of_particles) + 1 / (1 ** (self.num_of_particles - 2)) * kappa2_37_sum
 
-
             if acceptance_ratio >= 1 or ti.random(dtype=ti.f32) < acceptance_ratio:
                 self.mh_particles[i, j] = self.proposed_particles[i, j]
 def initialize_parameters():
     """Initialize and return user-defined parameters from the sidebar."""
-    num_of_particles = st.sidebar.number_input("Number of Particles", 1, 10000, 2)
-    a = st.sidebar.number_input("a", 0.0, 10.0, np.pi)
-    b = st.sidebar.number_input("b", 0.0, 1.0, 0.25)
-    c = st.sidebar.number_input("c", 0.0, 1.0, 0.1)
-    s = st.sidebar.number_input("s", 0.0, 1.0, 0.1)
-    proposal_std = st.sidebar.number_input("Proposal Standard Deviation", 0.0, 1.0, 0.1)
-    num_of_independent_trials = st.sidebar.number_input("Number of Independent Trials", 1, 1000000, 100)
-    num_of_iterations_for_each_trial = st.sidebar.number_input("Number of Iterations for Each Trial", 1, 1000000, 10)
-    each_particle = st.sidebar.checkbox("Visualize Each Particle Index", False)
-    save_image = st.sidebar.checkbox("Save Image", False)
-    return num_of_particles, a, b, c, s, proposal_std, num_of_independent_trials, num_of_iterations_for_each_trial, each_particle, save_image
+    st.session_state.num_of_particles = st.sidebar.number_input("Number of Particles", 1, 10000, 2)
+    st.session_state.a = st.sidebar.number_input("a", 0.0, 10.0, np.pi)
+    st.session_state.b = st.sidebar.number_input("b", 0.0, 1.0, 0.25)
+    st.session_state.c = st.sidebar.number_input("c", 0.0, 1.0, 0.1)
+    st.session_state.s = st.sidebar.number_input("s", 0.0, 1.0, 0.1)
+    st.session_state.proposal_std = st.sidebar.number_input("Proposal Standard Deviation", 0.0, 1.0, 0.1)
+    st.session_state.r_threshold = st.sidebar.number_input("r Threshold", 0.0, 1.0, 0.01)
+    st.session_state.num_of_independent_trials = st.sidebar.number_input("Number of Independent Trials", 1, 1000000, 100)
+    st.session_state.num_of_iterations_for_each_trial = st.sidebar.number_input("Number of Iterations for Each Trial", 1, 1000000, 10)
+    st.session_state.show_particles = st.sidebar.checkbox("Visualize Particles", False)
+    st.session_state.each_particle = st.sidebar.checkbox("Visualize Each Particle Index", False)
+    st.session_state.save_image = st.sidebar.checkbox("Save Image", False)
+    st.session_state.plotly = st.sidebar.checkbox("Use Plotly", False)
 
-def perform_calculations(params):
+    if 'mh_particles' not in st.session_state:
+        st.session_state.mh_particles = None
+
+    if 'distances' not in st.session_state:
+        st.session_state.distances = None
+
+def perform_calculations():
     """Perform the Metropolis-Hastings calculations and return the results."""
-    num_of_particles, a, b, c, s, proposal_std, num_of_independent_trials, num_of_iterations_for_each_trial, _, _ = params
-    MH = MetropolisHastings(num_of_particles, a, b, c, s, proposal_std, num_of_independent_trials)
+    MH = MetropolisHastings(
+        st.session_state.num_of_particles,
+        st.session_state.a,
+        st.session_state.b,
+        st.session_state.c,
+        st.session_state.s,
+        st.session_state.proposal_std,
+        st.session_state.num_of_independent_trials
+    )
     MH.initialize_particles()
 
     # Initial particles visualization setup
-    initial_particles = MH.init_particles.to_numpy()
-    colors = np.zeros((num_of_particles, 4))
-    for i in range(num_of_particles):
-        hue = i / float(num_of_particles)
+    st.session_state.initial_particles = MH.init_particles.to_numpy()
+    st.session_state.colors = np.zeros((st.session_state.num_of_particles, 4))
+    for i in range(st.session_state.num_of_particles):
+        hue = i / float(st.session_state.num_of_particles)
         rgb = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
-        colors[i] = rgb + (1.0,)
+        st.session_state.colors[i] = rgb + (1.0,)
 
     # Perform calculations
     taichi_start = time.time()
-    for _ in stqdm(range(num_of_iterations_for_each_trial)):
+    for _ in stqdm(range(st.session_state.num_of_iterations_for_each_trial)):
         MH.compute_mh()
-    taichi_end = time.time()
+    st.session_state.calc_time = time.time() - taichi_start
 
-    mh_particles = MH.mh_particles.to_numpy()
+    st.session_state.mh_particles = MH.mh_particles.to_numpy()
 
-    # Display the particles with plotly
-    df_list = []
-    for i, particles in enumerate(mh_particles):
-        df_temp = pd.DataFrame(particles, columns=['x', 'y'])
-        df_temp['Particle Index'] = i % num_of_particles  # num_of_particlesの数に応じてインデックスを割り当て
-        df_list.append(df_temp)
+    st.session_state.distances = []
+    for i in range(st.session_state.num_of_independent_trials):
+        # Calculate the distance between two particles
+        dist = toroidal_distance(1.0, st.session_state.mh_particles[i][0], st.session_state.mh_particles[i][1])
+        st.session_state.distances.append(dist)
 
-    df = pd.concat(df_list, ignore_index=True)
+    if st.session_state.plotly:
+        # Display the particles with plotly
+        df_list = []
+        for i, particles in enumerate(st.session_state.mh_particles):
+            df_temp = pd.DataFrame(particles, columns=['x', 'y'])
+            df_temp['Particle Index'] = i % st.session_state.num_of_particles
+            df_list.append(df_temp)
 
-    # Plotlyでプロットする
-    fig = px.scatter(df, x='x', y='y', color='Particle Index',  # ここでインデックスを色情報として使用
-                     title='Metropolis-Hastings Algorithm Sampling with Colored Particles',
-                     range_x=[0, 1], range_y=[0, 1], width=700, height=700,
-                     color_continuous_scale=px.colors.qualitative.G10)
-    fig.update_layout(yaxis_scaleanchor='x', xaxis_constrain='domain')
-    st.plotly_chart(fig, theme=None)
+        df = pd.concat(df_list, ignore_index=True)
 
-    calc_time = taichi_end - taichi_start
-    return initial_particles, colors, mh_particles, calc_time
+        fig = px.scatter(df, x='x', y='y', color='Particle Index',
+                         title='Metropolis-Hastings Algorithm Sampling with Colored Particles',
+                         range_x=[0, 1], range_y=[0, 1], width=700, height=700,
+                         color_continuous_scale=px.colors.qualitative.G10)
+        fig.update_layout(yaxis_scaleanchor='x', xaxis_constrain='domain')
+        st.plotly_chart(fig, theme=None)
 
 
 def draw_particles(data, colors, filename, size=(1000, 1000), save=False):
@@ -181,63 +199,82 @@ def draw_particles(data, colors, filename, size=(1000, 1000), save=False):
 
     return image
 
-def visualize_results(params, initial_particles, colors, particles):
+def visualize_particles():
     """Visualize and optionally save the results."""
-    num_of_particles, _, _, _, _, _, num_of_independent_trials, num_of_iterations_for_each_trial, each_particle, save_image = params
-
     # Visualize all initial particles
-    data_all = np.concatenate(initial_particles, axis=0)
-    # rgbaの4つの要素を持つ配列を作成
+    data_all = np.concatenate(st.session_state.initial_particles, axis=0)
     num_of_channels = 4
     colors_all = np.zeros((len(data_all), num_of_channels))
-    for i in range(num_of_particles):
-        hue = i / float(num_of_particles)
+    for i in range(st.session_state.num_of_particles):
+        hue = i / float(st.session_state.num_of_particles)
         rgb = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
-        colors_all[i::num_of_particles] = rgb + (1.0,)
-    image_all = draw_particles(data_all, colors_all, 'all_initial_particles', save=save_image)
+        colors_all[i::st.session_state.num_of_particles] = rgb + (1.0,)
+    image_all = draw_particles(data_all, colors_all, 'all_initial_particles', save=st.session_state.save_image)
     st.image(image_all)
 
     # Visualize all result particles
-    data_all = np.concatenate(particles, axis=0)
+    data_all = np.concatenate(st.session_state.mh_particles, axis=0)
     colors_all = np.zeros((len(data_all), num_of_channels))
-    for i in range(num_of_particles):
-        hue = i / float(num_of_particles)
+    for i in range(st.session_state.num_of_particles):
+        hue = i / float(st.session_state.num_of_particles)
         rgb = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
-        colors_all[i::num_of_particles] = rgb + (1.0,)
-    image_all = draw_particles(data_all, colors_all, 'all_result_particles', save=save_image)
+        colors_all[i::st.session_state.num_of_particles] = rgb + (1.0,)
+    image_all = draw_particles(data_all, colors_all, 'all_result_particles', save=st.session_state.save_image)
     st.image(image_all)
 
     # Visualize each particle index
-    if each_particle:
-        for i in range(num_of_particles):
-            data_index = np.array([chain[i] for chain in particles])
-            colors_index = colors_all[i::num_of_particles]
-            filename = f'particles_index_{i}_trial_count_{num_of_independent_trials}_mh_steps_{num_of_iterations_for_each_trial}'
-            image_index = draw_particles(data_index, colors_index, filename, save=save_image)
+    if st.session_state.each_particle:
+        for i in range(st.session_state.num_of_particles):
+            data_index = np.array([chain[i] for chain in st.session_state.mh_particles])
+            colors_index = colors_all[i::st.session_state.num_of_particles]
+            filename = f'particles_index_{i}_trial_count_{st.session_state.num_of_independent_trials}_mh_steps_{st.session_state.num_of_iterations_for_each_trial}'
+            image_index = draw_particles(data_index, colors_index, filename, save=st.session_state.save_image)
             st.image(image_index)
 
-    # 2粒子間の距離を計算
-    distances = []
-    for i in range(num_of_independent_trials):
-        dist = toroidal_distance_cpu(1.0, particles[i][0], particles[i][1])
-        distances.append(dist)
+def visualize_histogram():
 
-
-    # 2粒子間の距離のヒストグラムをプロット
-    fig = px.histogram(distances, title='Distance between Two Particles', nbins=1000)
-    fig.update_layout(xaxis_title='Distance', yaxis_title='Frequency')
+    # Display the histogram with go
+    fig = go.Figure(data=[go.Histogram(x=st.session_state.distances, nbinsx=50)])
+    fig.update_layout(title='Distance between Two Particles', xaxis_title='Distance', yaxis_title='Frequency')
     st.plotly_chart(fig, theme=None)
 
+    # Normalize the histogram
+    hist, bin_edges = np.histogram(st.session_state.distances, bins=50)
+    print("hist", hist)
+    print("bin_edges", bin_edges)
+    bin_centers = 0.5 * (bin_edges[:-1] + bin_edges[1:])
+    print("bin_centers", bin_centers)
+
+    # r threshold is set to avoid division by zero and ignore small distances
+    valid_indices = bin_centers > st.session_state.r_threshold
+    print("valid_indices", valid_indices)
+    filtered_bin_centers = bin_centers[valid_indices]
+    print("filtered_bin_centers", filtered_bin_centers)
+    filtered_hist = hist[valid_indices]
+    print("filtered_hist", filtered_hist)
+
+    normalized_hist = filtered_hist / filtered_bin_centers
+
+    print("normalized_hist", normalized_hist)
+
+    fig = go.Figure(data=[go.Bar(x=bin_edges, y=normalized_hist)])
+    fig.update_layout(title='Normalized Distance between Two Particles', xaxis_title='Distance', yaxis_title='Normalized Frequency')
+    st.plotly_chart(fig, theme=None)
 
 
 def main():
     st.title('Metropolis-Hastings Algorithm Sampling')
-    params = initialize_parameters()
+    initialize_parameters()
 
     if st.button('Calculate'):
-        initial_particles, colors, mh_particles, calc_time = perform_calculations(params)
-        st.info(f"Calculation Time: {calc_time:.2f} sec")
-        visualize_results(params, initial_particles, colors, mh_particles)
+        perform_calculations()
+        st.info(f"Calculation Time: {st.session_state.calc_time:.2f} sec")
+
+    if st.session_state.mh_particles is not None and st.session_state.show_particles:
+        visualize_particles()
+
+    if st.session_state.distances is not None:
+        visualize_histogram()
 
 if __name__ == "__main__":
     ti.init(arch=ti.cpu, random_seed=int(time.time()))
